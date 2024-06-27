eaa2151d7000acebd35070a11ee91c22fb5b11f6
brunonicvl <brunonicladosky@gmail.com>
2024-06-26 23:42:32 -0300

    Create EscritorTiposDespesaDAO.java

---
+++ src/usp/mac321/ep2/EscritorTiposDespesaDAO.java
@@ -0,0 +1,7 @@
+package usp.mac321.ep2;
+
+import java.util.*;
+
+public interface EscritorTiposDespesaDAO {
+	public void escreveTiposDespesa(List<TipoDespesa> l, String nomeArquivoTiposDespesa);
+}
