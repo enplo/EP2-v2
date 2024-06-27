8f61801d66cc4110c5bdc8cf3982d04ee7c313a1
brunonicvl <brunonicladosky@gmail.com>
2024-06-26 23:31:40 -0300

    Update LeitorFinancasPessoais.java

--- src/usp/mac321/ep2/LeitorFinancasPessoais.java
+++ src/usp/mac321/ep2/LeitorFinancasPessoais.java
@@ -1,5 +1,6 @@
 package usp.mac321.ep2;
 
+
 import java.util.*;
 
 import java.io.*;
@@ -19,23 +20,19 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 	
 	private boolean repetido(String apelido, List<Usuario> listaUsuarios) throws ApelidoRepetidoException {
 		if (!listaUsuarios.isEmpty()) {
-		for (int i = 0; i < listaUsuarios.size(); i++) {
-			if (listaUsuarios.get(i).getApelido().equals(apelido)) {
-				throw new ApelidoRepetidoException(apelido);
-			}	
-			
-			
-		}
-		
+			for (int i = 0; i < listaUsuarios.size(); i++) {
+				if (listaUsuarios.get(i).getApelido().equals(apelido)) {
+					throw new ApelidoRepetidoException(apelido);
+				}				
+			}
 		}
 		return false;
 		
 	}
 	
 	
-	//@Override
+	@Override
 	public List<Usuario> leUsuarios(String nomeArquivoUsuarios){
-		//List<Usuario> listaUsuarios = new ArrayList<Usuario>();
 		usuarios = new ArrayList<Usuario>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(nomeArquivoUsuarios));
@@ -44,25 +41,22 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 			line = br.readLine();
 			do {
 				line = br.readLine();
-				if (line != null && !repetido(line.split(",")[0],usuarios)) {
-					usuarios.add(new Usuario(line.split(",")[0], line.split(",")[1]));
-					
+				if(line!=null) {
+					String[] lineSplit = line.split(",");
+					if (line != null && !repetido(lineSplit[0], usuarios)) {
+						usuarios.add(new Usuario(lineSplit[0], lineSplit[1]));
+					}
 				}
-				
-			} 
-			
-			
-			while(line != null);
+			} while(line != null);
 				br.close();
 		}
 		
 		catch (FileNotFoundException e) {
 			System.err.println("Arquivo não encontrado");
 
-		} // Não consegui encontrar se IOException também serve
-		  //para isso, mas encontrei uma exceção exatamente para esse problema
+		}
 		
-		catch (IOException e) { //pesquisar se isso se aplica a não existir nome de arquivo!!
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 		
@@ -70,51 +64,44 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 			System.err.print(e.toString());
 		}
 		
-		//usuarios = listaUsuarios;
 		return usuarios;
 	}
 
-	//@Override
+	@Override
 	public List<TipoDespesa> leTiposDespesas(String nomeArquivoTiposDespesas) {
-		//List<TipoDespesa> listaTiposDespesas = new ArrayList<TipoDespesa>();
 		despesas = new ArrayList<TipoDespesa>();
-		
 		
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(nomeArquivoTiposDespesas));
 			String line = null;
 			
-			line = br.readLine(); // Descarta a primeira linha do arquivo
+			line = br.readLine();
 			do {
 				line = br.readLine();
-				//Espera-se linha do tipo "Tipo,Subcategoria,Subcategoria,Subcategoria,..."
-				//Determina-se algo como Subcategoria se for descrita por algum outro tipo em qualquer linha
 				if (line != null) {
 					if(line.contains(",")) {
 						String[] lineSplit = line.split(",");
 						List<TipoDespesa> subtiposDespesa = new ArrayList<TipoDespesa>();
 						
 						TipoDespesa tipoPrincipal;
-						/*
-						try {
-							tipoPrincipal = getTipoDespesaFromList(lineSplit[0], listaTiposDespesas);
-						}
-						catch(TipoNaoRegistradoException e) {
-							tipoPrincipal = new TipoDespesa(lineSplit[0], TIPO_PRINCIPAL);
-							//listaTiposDespesas.add(tipoPrincipal);
-						}*/
-						
 						tipoPrincipal = new TipoDespesa(lineSplit[0], TIPO_PRINCIPAL);
+						for(int i = 0; i<despesas.size(); i++) {
+							if(despesas.get(i).getNome().equals(lineSplit[0])) {
+								tipoPrincipal = despesas.get(i);
+								subtiposDespesa = despesas.get(i).getListaSubcategorias();
+								despesas.remove(i);
+							}
+						}
 						
 						for(int i=1; i<lineSplit.length; i++) {
 							TipoDespesa novoTipo;
 							try {
 								novoTipo = getTipoDespesaFromList(lineSplit[i], despesas);
-								novoTipo.sub = SUBCATEGORIA;
+								novoTipo.setSub(SUBCATEGORIA);
 							}
 							catch(TipoNaoRegistradoException e) {
 								novoTipo = new TipoDespesa(lineSplit[i], SUBCATEGORIA);
-								//listaTiposDespesas.add(novoTipo);
+								despesas.add(novoTipo);
 							}
 							subtiposDespesa.add(novoTipo);
 							novoTipo.setTipoDerivado(tipoPrincipal);
@@ -135,14 +122,14 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 			System.err.println("Arquivo não encontrado");
 
 		} 
-		catch (IOException e) { //pesquisar se isso se aplica a não existir nome de arquivo!!
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 		return despesas;
 	}
 
-	//@Override
+	@Override
 	public List<TipoReceita> leTiposReceitas(String nomeArquivoTiposReceitas) {
 		receitas = new ArrayList<TipoReceita>();
 		
@@ -153,33 +140,30 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 			line = br.readLine();
 			do {
 				line = br.readLine();
-				//Espera-se linha do tipo "Tipo,Subcategoria,Subcategoria,Subcategoria,..."
-				//Determina-se algo como Subcategoria se for descrita por algum outro tipo em qualquer linha,
-				//Mesmo que haja uma linha descrevendo-a como tendo suas próprias subcategorias
 				if (line != null) {
 					if(line.contains(",")) {
 						String[] lineSplit = line.split(",");
 						List<TipoReceita> subtiposReceita = new ArrayList<TipoReceita>();
 						
 						TipoReceita tipoPrincipal;
-						/*try {
-							tipoPrincipal = getTipoReceitaFromList(lineSplit[0], listaTiposReceitas);
-						}
-						catch(TipoNaoRegistradoException e) {
-							tipoPrincipal = new TipoReceita(lineSplit[0], TIPO_PRINCIPAL);
-							//listaTiposReceitas.add(tipoPrincipal);
-						}*/
 						tipoPrincipal = new TipoReceita(lineSplit[0], TIPO_PRINCIPAL);
+						for(int i = 0; i<receitas.size(); i++) {
+							if(receitas.get(i).getNome().equals(lineSplit[0])) {
+								tipoPrincipal = receitas.get(i);
+								subtiposReceita = receitas.get(i).getListaSubcategorias();
+								receitas.remove(i);
+							}
+						}
 						
 						for(int i=1; i<lineSplit.length; i++) {
 							TipoReceita novoTipo;
 							try {
 								novoTipo = getTipoReceitaFromList(lineSplit[i], receitas);
-								novoTipo.sub = SUBCATEGORIA;
+								novoTipo.setSub(SUBCATEGORIA);
 							}
 							catch(TipoNaoRegistradoException e) {
 								novoTipo = new TipoReceita(lineSplit[i], SUBCATEGORIA);
-								//listaTiposReceitas.add(novoTipo);
+								receitas.add(novoTipo);
 							}
 							subtiposReceita.add(novoTipo);
 							novoTipo.setTipoDerivado(tipoPrincipal);
@@ -202,16 +186,15 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 
 		}
 		
-		catch (IOException e) { //pesquisar se isso se aplica a não existir nome de arquivo!!
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 		return receitas;
 	}
 
-	//@Override
+	@Override
 	public List<Lancamento> leLancamentos(String nomeArquivoLancamentos) {
-		//List<Lancamento> listaLancamentos = new ArrayList<Lancamento>();
 		lancamentos = new ArrayList<Lancamento>();
 		
 		try {
@@ -222,46 +205,61 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 			do {
 				line = br.readLine();
 				if (line != null) {
-					//Lancamentos serão salvos como uma linha do tipo
-					//dia, mes, ano, user, receitaOuDespesa, tipo, descricao, valor
 					int identificador;
 					int dia, mes, ano;
 					String[] lineSplit = line.split(",");
+					
 					String[] dataSplit = lineSplit[1].split("/");
-					System.out.println(lineSplit);
 					identificador = Integer.parseInt(lineSplit[0]);
 
 					dia = Integer.parseInt(dataSplit[0]);
 					mes = Integer.parseInt(dataSplit[1]);
 					ano = Integer.parseInt(dataSplit[2]);
 					
-					if(dia<=0) throw new ValorNegativoException(dia);
-					if(mes<=0) throw new ValorNegativoException(mes);
-					if(ano<=0) throw new ValorNegativoException(ano);
+					if(dia<=0) {
+						throw new ValorNegativoException(dia);
+					}
+					if(mes<=0) {
+						throw new ValorNegativoException(mes);
+					}
+					if(ano<=0) {
+						throw new ValorNegativoException(ano);
+					}
 					
 					Usuario user;
-					user = getUsuarioFromList(lineSplit[3], usuarios);
+					user = getUsuarioFromList(lineSplit[2], usuarios);
 					
 					boolean rOuD;
-					if(lineSplit[5].equals("TRUE")) {
+					if(lineSplit[3].equals("TRUE")) {
 						rOuD = DESPESA;
 					}
-					else if(lineSplit[5].equals("FALSE")) {
+					else if(lineSplit[3].equals("FALSE")) {
 						rOuD = RECEITA;
 					}
-					
 					else {
-						throw new ValorInvalidoException(lineSplit[5]);
+						throw new ValorInvalidoException(lineSplit[3]);
 					}
 					
-					TipoOperacao tipo;
-					try {
-						tipo = getTipoReceitaFromList(lineSplit[6], receitas);
+					TipoOperacao tipo = null;
+					boolean tipoNaoEncontrado = true;
+					for(TipoReceita i: receitas) {
+						if(i.getNome().equals(lineSplit[4])) {
+							tipo = i;
+							tipoNaoEncontrado = false;
+						}
 					}
-					catch(TipoNaoRegistradoException e){
-						tipo = getTipoDespesaFromList(lineSplit[6], despesas);
-						System.out.println("IOException: " + e.getMessage());
+					if(tipoNaoEncontrado) {
+						for(TipoDespesa i: despesas) {
+							if(i.getNome().equals(lineSplit[4])) {
+								tipo = i;
+								tipoNaoEncontrado = false;
+							}
+						}
 					}
+					if(tipoNaoEncontrado) {
+						throw new TipoNaoRegistradoException(lineSplit[4]);
+					}
+					
 					if(tipo.getClass().equals(TipoReceita.class) && (rOuD==DESPESA) ||
 							tipo.getClass().equals(TipoDespesa.class) && (rOuD==RECEITA)) {
 						throw new ConfusaoReceitaDespesaException(lineSplit[5]);
@@ -270,11 +268,10 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 					String descricao = lineSplit[6];
 					
 					double valor;
-					valor = Double.parseDouble(lineSplit[7]);
-					if(valor<=0) throw new ValorNegativoException(1);
-					
-					
-					
+					valor = Double.parseDouble(lineSplit[5]);
+					if(valor<=0) {
+						throw new ValorNegativoException(Double.parseDouble(lineSplit[5]));
+					}
 					if(identificador<=0) throw new ValorNegativoException(identificador);
 					for(Lancamento i: lancamentos) {
 						if(i.getID()==identificador) {
@@ -283,23 +280,17 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 					}
 					
 					lancamentos.add(new Lancamento(dia, mes, ano, user, rOuD, tipo, descricao, valor, identificador));
-					System.out.println(1234);
 				}
 			} while(line != null);
 			br.close();
 		}
-		catch (IOException e) { //pesquisar se isso se aplica a não existir nome de arquivo!!
+		catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("IOException1: " + e.getMessage());
 		}
-		/*catch (FileNotFoundException e) { Talvez n'ao precise dessa exception
-			System.err.println("Arquivo não encontrado");
-		}*/
-		
-		
 		catch (Exception e) { //pesquisar se isso se aplica a não existir nome de arquivo!!
 			e.printStackTrace();
-			System.out.println("IOException2: " + e.getMessage());
+			System.out.println("IOException2: " + e.toString());
 		}
 		
 		return lancamentos;
@@ -314,16 +305,6 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 		throw new TipoNaoRegistradoException(nome);
 	}
 	
-	public TipoDespesa getTipoDespesaFromList(String nome) throws TipoNaoRegistradoException {
-		for(TipoDespesa i: despesas) {
-			if(i.getNome().equals(nome)) {
-				return i;
-			}
-		}
-		throw new TipoNaoRegistradoException(nome);
-	}
-
-
 	public TipoReceita getTipoReceitaFromList(String nome, List<TipoReceita> l) throws TipoNaoRegistradoException {
 		for(TipoReceita i: l) {
 			if(i.getNome().equals(nome)) {
@@ -332,17 +313,7 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 		}
 		throw new TipoNaoRegistradoException(nome);
 	}
-
-	public TipoReceita getTipoReceitaFromList(String nome) throws TipoNaoRegistradoException {
-		for(TipoReceita i: receitas) {
-			if(i.getNome().equals(nome)) {
-				return i;
-			}
-		}
-		throw new TipoNaoRegistradoException(nome);
-	}
 	
-	/*
 	public Usuario getUsuarioFromList(String apelido, List<Usuario> l) throws UsuarioNaoExistenteException {
 		if (l != null){
 		for(Usuario i: l) {
@@ -351,25 +322,6 @@ public class LeitorFinancasPessoais implements LeitorFinancasPessoaisDAO {
 			}
 		}}
 		throw new UsuarioNaoExistenteException(apelido);
-	}*/
+	}
 	
-	public Usuario getUsuarioFromList(String apelido, List<Usuario> l) throws UsuarioNaoExistenteException {
-		if (l != null){
-		for(Usuario i: l) {	
-			if(i.getApelido().equals(apelido)) {
-				return i;
-			}
-		}}
-		throw new UsuarioNaoExistenteException(apelido);
-	}
-
-	public Usuario getUsuarioFromList(String apelido) throws UsuarioNaoExistenteException {
-		if (usuarios != null){
-		for(Usuario i: usuarios) {	
-			if(i.getApelido().equals(apelido)) {
-				return i;
-			}
-		}}
-		throw new UsuarioNaoExistenteException(apelido);
-	}
 }
